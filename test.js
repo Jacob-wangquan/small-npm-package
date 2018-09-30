b176bab0cc66d3812eb6f18afe6e78e035fb0e32
汪全海 <jacob.wang@huolala.cn>
2018-09-30 14:25:52 +0800

    array methods twice

--- test.js
+++ test.js
@@ -26,3 +26,38 @@ var flatten = require('arr-flatten');
 //7,移除重复的元素
 var dedupe = require('dedupe');
 //console.log(dedupe([1,3,3,5,6]));
+//8,创建一个特定长度的数组
+var range = require('array-range');
+// console.log(range(3));
+// console.log(range(1,5));
+// console.log(range('1',5));
+//9,取出第一个数组中有而第二个数组中没有的元素
+var diff = require('arr-diff');
+//console.log(diff(['a','b','cccc'], ['a', 'c']));
+//10,用元素或者函数按规则 创建数组
+var filledArray = require('filled-array');
+// console.log(filledArray('a', 3));
+// console.log(filledArray( i => {
+//   return (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i;
+// }, 10));
+
+//11,Map object keys and values into a new object
+var mapObject = require('map-obj');
+//console.log(mapObject({foo: 'bar'}, (key, value) => [value, key]));
+
+//Map object keys and values into an array
+var mapArray = require('map-array');
+var obj = {
+  giorgio: 'bianchi',
+  gibo: 'rossi'
+};
+// console.log(mapArray(obj, function(key, value){
+//   return key + '-' + value;
+// }));
+//判断元素是否在数组中
+var inArray = require('in-array');
+//console.log(inArray(['a', 'b', 'c'], 'b'));
+
+//不使用splice，从一个无序数组中快速移除一个元素
+var remove = require('unordered-array-remove');
+console.log(remove(['a', 'b', 'c'], 2));
